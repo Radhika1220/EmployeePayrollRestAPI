1f85442b8de0403204473f663a9c38430f20a42c
Radhika <radhika.shankar1220@gmail.com>
2021-08-07 14:19:55 +0530

    [Radhika] Getting the employee details from json

---
+++ PayrollRestAPITest/Employee.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PayrollRestAPITest
+{
+   public  class Employee
+    {
+        public int id { get; set; }
+         public string name { get; set; }
+        public float salary { get; set; }
+    }
+}
